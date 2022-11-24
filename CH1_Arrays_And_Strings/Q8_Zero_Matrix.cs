c83658d062e96d14cc6f79c505a0f89a7f327d2c
Mateus Mannes De Medeiros <mateus-mannes@hotmail.com>
2022-11-24 08:02:52 -0300

    .

--- CH1_Arrays_And_Strings/Q8_Zero_Matrix.cs
+++ CH1_Arrays_And_Strings/Q8_Zero_Matrix.cs
@@ -23,6 +23,44 @@ public static class ZeroMatrix {
         }
     }
 
+    // runs without extra memory
+    public static void Run2(int[,] matrix, int N){
+        bool rowHasZero = false;
+        bool columnHasZero = false;
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < N; j++){
+                if(i == 0 && matrix[i,j] == 0) rowHasZero = true;
+                if(j == 0 && matrix[i,j] == 0) columnHasZero = true;
+                if(i == 0 || j == 0) continue;
+                if(matrix[i,j] == 0) {
+                    matrix[0,j] = 0;
+                    matrix[i, 0] = 0;
+                }
+            }
+        }
+        for(int i = 1; i < N; i++)
+            if(matrix[0, i] == 0) NullColumn(matrix, i);
+
+        for(int i = 1; i < N; i++)
+            if(matrix[i, 0] == 0) NullRow(matrix, i);
+
+        if(rowHasZero) NullRow(matrix, 0);
+        if(columnHasZero) NullColumn(matrix, 0);
+        if(rowHasZero || columnHasZero) matrix[0,0] = 0;
+    }
+
+    private static void NullRow(int[,] matrix, int row){
+        for(int j =1; j < matrix.GetLength(1); j++){
+            matrix[row, j] = 0; 
+        }
+    }
+
+    private static void NullColumn(int[,] matrix, int column){
+        for(int i =1; i < matrix.GetLength(0); i++){
+            matrix[i, column] = 0; 
+        }
+    }
+
     
 
 }
