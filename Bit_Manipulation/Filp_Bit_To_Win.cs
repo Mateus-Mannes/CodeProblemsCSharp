ddeca75b83ab7b9a255a2a663535ef20d7064bfe
Mateus Mannes De Medeiros <mateus.medeiros@becomex.com.br>
2023-01-16 20:37:07 -0300

    .

---
+++ Bit_Manipulation/Filp_Bit_To_Win.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bit_Manipulation
+{
+    public static class Filp_Bit_To_Win
+    {
+        public static int Run(int number)
+        {
+            int biggerSequence = 0;
+            int left = 0;
+            int sum = 0;
+            for (int i = 1 << 30; i > 0; i /= 2)
+            {
+                if((number & i) > 0)
+                {
+                    sum++;
+                }
+                else
+                {
+                    if (left == 0) left = sum;
+                    else
+                    {
+                        if((left + sum + 1) > biggerSequence) biggerSequence = (left + sum + 1);
+                        left = sum;
+                    }
+                    sum = 0;
+                }
+                
+            }
+            if ((left + sum + 1) > biggerSequence) biggerSequence = (left + sum + 1);
+            return biggerSequence;
+        }
+    }
+}
