ff8c93533da375d2b5dba0b8b62745fb264e8b45
Mateus Mannes De Medeiros <mateus.medeiros@becomex.com.br>
2023-01-17 21:52:42 -0300

    .

---
+++ Bit_Manipulation/Conversion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bit_Manipulation
+{
+    public static class Conversion
+    {
+        public static int Run(int n1, int n2)
+        {
+            int flips = 0;
+            for(int i = 1 << 30; i > 0; i /= 2)
+            {
+                if( ((n1 & i) == 0) && ((n2 & i) > 0) || ((n1 & i) > 0) && ((n2 & i) == 0))
+                    flips++;
+            }
+            return flips;
+        }
+    }
+}
---
+++ Bit_Manipulation/Parwise_Swap.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bit_Manipulation
+{
+    public static class Parwise_Swap
+    {
+        public static void Run(int number)
+        {
+            PrintBits.Print(number);
+            int even = 178956970;
+            int odd = 89478485;
+            int left = (number & odd) << 1;
+            int right = (number & even) >> 1;
+            PrintBits.Print(left | right);
+        }
+    }
+}
