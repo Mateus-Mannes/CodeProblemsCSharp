585fee59e9b4bfc6406d5a08ced4f48cd7adf19a
Mateus Mannes De Medeiros <mateus.medeiros@becomex.com.br>
2023-02-02 07:58:13 -0300

    .

---
+++ Recursion/RecursiveMultiply.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursion
+{
+    public static class RecursiveMultiply
+    {
+        public static int Run(int smaller, int bigger)
+        {
+            if(smaller == 0) return 0;
+            if (smaller == 1) return bigger;
+
+            int s = smaller >>1;
+            int halfProd = Run(s, bigger);
+
+            if(smaller % 2 == 0)
+            {
+                return halfProd + halfProd;
+            }else
+            {
+                return halfProd + halfProd + bigger;
+            }
+        }
+    }
+}
