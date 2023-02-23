80f2f183acae13d16678669b74966228416f4b3a
Mateus Mannes De Medeiros <mateus.medeiros@becomex.com.br>
2023-02-23 06:59:43 -0300

    .

---
+++ Recursion/Permutation_No_Dups.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursion
+{
+    public static class Permutation_No_Dups
+    {
+        public static List<string> GetPermutations(string word)
+        {
+            if(word.Length == 1) return new List<string> { word };
+            char lastChar = word.Last();
+            var subPermutations = GetPermutations(word.Substring(0, word.Length - 1));
+            var permutations = new List<string>();
+            foreach(var permutation in subPermutations)
+            {
+                for(int i = 0; i <= permutation.Length; i++)
+                {
+                    permutations.Add(permutation.Insert(i, lastChar.ToString()));
+                }
+            }
+            return permutations;
+        }
+    }
+}
