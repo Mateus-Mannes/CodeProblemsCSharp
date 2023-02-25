c2d329a99b6e614ebd2dea2bf02b0958ef26e770
Mateus Mannes De Medeiros <mateus.medeiros@becomex.com.br>
2023-02-25 14:40:26 -0300

    .

---
+++ Recursion/Permutation_Dups.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursion
+{
+    public static class Permutation_Dups
+    {
+        public static List<string> GetPerm(string word)
+        {
+            var result = new List<string>();
+            var map = MakeMap(word);
+            GetPerm(map, "", word.Length, result);
+            return result;
+        }
+
+        public static void GetPerm(Dictionary<char, int> map, string prefix, int remaining, List<string> result)
+        {
+            if(remaining == 0)
+            {
+                result.Add(prefix);
+                return;
+            }
+            foreach(var pair in map)
+            {
+                int count = pair.Value;
+                if(count > 0)
+                {
+                    map[pair.Key]--;
+                    GetPerm(map, prefix + pair.Key, remaining - 1, result);
+                    map[pair.Key]++;
+
+                }
+            }
+        }
+        
+        public static Dictionary<char, int> MakeMap(string word)
+        {
+            var map = new Dictionary<char, int>();
+            foreach(var c in word)
+            {
+                if (map.ContainsKey(c)) map[c]++;
+                else map.Add(c, 1);
+            }
+            return map;
+        }
+    }
+}
--- Recursion/Permutation_No_Dups.cs
+++ Recursion/Permutation_No_Dups.cs
@@ -8,6 +8,8 @@ namespace Recursion
 {
     public static class Permutation_No_Dups
     {
+        public static HashSet<char> chars = new HashSet<char>();
+
         public static List<string> GetPermutations(string word)
         {
             if(word.Length == 1) return new List<string> { word };
@@ -19,6 +21,24 @@ namespace Recursion
                 for(int i = 0; i <= permutation.Length; i++)
                 {
                     permutations.Add(permutation.Insert(i, lastChar.ToString()));
+                }
+            }
+            return permutations;
+        }
+
+        public static List<string> GetPermutationsDups(string word)
+        {
+            if (word.Length == 1) return new List<string> { word };
+            char lastChar = word.Last();
+            var subPermutations = GetPermutationsDups(word.Substring(0, word.Length - 1));
+            var permutations = new List<string>();
+            foreach (var permutation in subPermutations)
+            {
+                for (int i = 0; i <= permutation.Length; i++)
+                {
+                    string newWord = permutation.Insert(i, lastChar.ToString());
+                    if (permutations.Count > 0 && permutations.Last() == newWord) continue;
+                    permutations.Add(newWord);
                 }
             }
             return permutations;
