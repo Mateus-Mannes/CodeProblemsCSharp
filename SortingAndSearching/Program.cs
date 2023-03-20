d978349f6f6250a4a856fbf68f393335cd366715
Mateus Mannes De Medeiros <mateus-mannes@hotmail.com>
2023-03-20 07:20:23 -0300

    .

--- SortingAndSearching/Program.cs
+++ SortingAndSearching/Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-var words = new List<string>() { "abcd", "tio", "bcda", "oit", "cdab"};
-var result = Anagrams.SortByAnagram(words);
-var x = 1;
+using SortingAndSearching;
+
+var l = new List<int>() { 1, 2, 3, 4, 5, 12, 43, 543, 786, 987, 4353, 65446};
+
+Console.WriteLine(Sorted_Search.SortedSearch(l, 43));
---
+++ SortingAndSearching/SortedSearch.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingAndSearching
+{
+    public static class Sorted_Search
+    {
+        public static int SortedSearch(List<int> list, int x)
+        {
+            if (list.ElementAt2(0) == x) return 0;
+            int len = 1;
+            while(list.ElementAt2(len) > 0 && list.ElementAt2(len) < x) len *= 2;
+            return BinarySerch(list, len / 2, len, x);
+        }
+
+        public static int BinarySerch(List<int> list, int ini, int fin, int x)
+        {
+            if (ini > fin) return -1;
+            int mid = (ini + fin) / 2;
+            if (list.ElementAt(mid) > x || list.ElementAt(mid) == -1) 
+                return BinarySerch(list, ini, mid -1, x);
+            else if(list.ElementAt(mid) < x) 
+                return BinarySerch(list, mid + 1, fin, x);
+            else return mid;
+        }
+
+        public static int ElementAt2(this List<int> list, int x)
+        {
+            try
+            {
+                return list.ElementAt(x);
+            }
+            catch
+            {
+                return -1;
+            }
+        }
+    }
+}
